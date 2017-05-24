749d6abbe2653cc509fba5ca7955c4633ef9fadd
Maarten <digitalheir@users.noreply.github.com>
2017-05-24 15:42:01 +0200

    Update BibStringItem.ts

--- src/bibfile/BibStringItem.ts
+++ src/bibfile/BibStringItem.ts
@@ -11,6 +11,13 @@ export class BibStringItem {
     }
 }
 
+/**
+ * A special character is a
+part of a field starting with a left brace being at brace depth 0 immediately followed with a backslash,
+and ending with the corresponding right brace. For instance, in the above example, there is no special
+character, since \LaTeX is at depth 2. It should be noticed that anything in a special character is
+considered as being at brace depth 0, even if it is placed between another pair of braces.
+ */
 export class SpecialCharacter extends BibStringItem {
     constructor(type: string, braceDepth: number) {
         super("specialCharacter", 0)
