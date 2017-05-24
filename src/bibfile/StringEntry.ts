7d5106169ebfca8bf5a0e071e7d2e8192adf5e92
Maarten <digitalheir@users.noreply.github.com>
2017-05-24 11:11:30 +0200

    Update StringEntry.ts

--- src/bibfile/StringEntry.ts
+++ src/bibfile/StringEntry.ts
@@ -13,6 +13,17 @@ export class StringEntry extends BibFileNode {
     }
 }
 
+export class ResolvedStringEntry extends StringEntry {
+    readonly resolvedValue: Stringy[];
+
+    public constructor(key: string, value: Stringy[], resolvedValue: Stringy[]) {
+        super("string");
+        this.key = key;
+        this.value = value;
+        this.resolvedValue = resolvedValue;
+    }
+}
+
 function findKeyVal(data: any): KeyVal {
     if (isKeyVal(data)) {
         return newKeyVal(data);
@@ -22,6 +33,34 @@ function findKeyVal(data: any): KeyVal {
         }
         return findKeyVal(data.data);
     }
+}
+
+export function resolveStringObjects(
+                           seenBeforeStack: {[key: string]: boolean},
+                           compiledSoFar: {[key: string]: ResolvedStringEntry},
+                           rawStrings: Abbreviations,
+                           strObj: any
+    ) {
+    if (typeof strObj === 'object' && strObj.stringref) {
+        const refName = strObj.stringref;
+        if (seenBeforeStack[refName])
+            throw new Error("Cycle detected: " + refName);
+        if (compiledSoFar[refName])
+            return compiledSoFar[refName];
+        if (!rawStrings[refName])
+            throw new Error("Unresolved reference: " + JSON.stringify(strObj));
+        //console.log("RESOLVE", refName);
+        compiledSoFar[refName] = resolveStringDeclarations(
+            Object.assign({}, seenBeforeStack, {[refName]: true}),
+            rawStrings[refName],
+            compiledSoFar,
+            rawStrings
+        );
+        return compiledSoFar[refName];
+    } else if (strObj._raw)
+        return strObj;
+    else
+        return strObj;
 }
 
 export function newStringNode(data: any): StringEntry {
