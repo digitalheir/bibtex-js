ac4ae7636c4eb622eee76afbe60f02fa4ad3d432
Maarten <digitalheir@users.noreply.github.com>
2017-05-24 15:50:40 +0200

    Update BibStringItem.ts

--- src/bibfile/BibStringItem.ts
+++ src/bibfile/BibStringItem.ts
@@ -1,13 +1,16 @@
 export class BibStringItem {
+    readonly data: BibStringItem[];
     readonly type: string;
+
     /**
      * The brace depth of an item is the number of braces surrounding it (surrounding the field with braces instead of quotes does not modify the brace depth)
      */
     readonly braceDepth: number;
 
-    constructor(type: string, braceDepth: number) {
+    constructor(type: string, braceDepth: number, data: BibStringItem[]) {
         this.type = type;
         this.braceDepth = braceDepth;
+        this.data = data;
     }
 }
 
@@ -19,7 +22,18 @@ character, since \LaTeX is at depth 2. It should be noticed that anything in a s
 considered as being at brace depth 0, even if it is placed between another pair of braces.
  */
 export class SpecialCharacter extends BibStringItem {
-    constructor(type: string, braceDepth: number) {
-        super("specialCharacter", 0)
+    constructor(data: BibStringItem[]) {
+        super("specialCharacter", 0, data);
+    }
+}
+
+// TODO where2Heck put this?
+export function convert(obj: any, braceDepth: number) {
+    if(obj.type === "braced) {
+        if (braceDepth === 0 && isArray(obj.data) && obj.data[0] === "\\"){
+            return new SpecialCharacter(data);
+        }else{
+            return new BracedString(braceDepth, data);            
+        }
     }
 }
