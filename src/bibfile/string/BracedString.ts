05301afb8913adefd70e6e2c2994fce26008bd9f
Maarten <digitalheir@users.noreply.github.com>
2017-05-26 12:21:49 +0200

    Update BracedString.ts

--- src/bibfile/string/BracedString.ts
+++ src/bibfile/string/BracedString.ts
@@ -1,8 +1,13 @@
 import {BibOuterStringComponent, BibStringComponent, BibStringData} from "./BibStringItem";
 
 export class BracedString extends BibStringComponent {
+    readonly isSpecialCharacter: boolean;
+    
     constructor(braceDepth: number, data: BibStringData) {
         super("bracedstring", braceDepth, data);
+        
+        // TODO braced strings inside a special character is treated as if it has brace depth 0. Maybe it's a good idea to mark these nested braces?
+        this.isSpecialCharacter = braceDepth === 0 && data[0] === "\\";
     }
 
 }
