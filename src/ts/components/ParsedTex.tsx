4f2e2dadfcaf95797c3b645ebc40c2c15208a239
Maarten Trompper <maartentrompper@freedom.nl>
2017-07-06 22:59:37 +0200

    v0.3.0

--- src/ts/components/ParsedTex.tsx
+++ src/ts/components/ParsedTex.tsx
@@ -1,63 +1,92 @@
 import * as React from "react";
-import {PureComponent} from "react";
+import {StatelessComponent} from "react";
+import {Parser, Result} from "parsimmon";
 
 import {
-    LatexStyle,
-    LatexParser,
-    Token,
-    Node as TexNode
+    isTeXBraces,
+    isTeXComm,
+    isTeXComment,
+    isFixArg,
+    isOptArg,
+    isTeXEnv, isTeXLineBreak, isTeXMath,
+    isTeXRaw,
+    LaTeX,
+    TeXArg
 } from "latex-parser";
 
+const Arguments: StatelessComponent<{ args: TeXArg[] }> = ({args}) => <span>
+    {[].concat(...args.map((e, i) => [", ", <span key={i}>{renderTokens(e)}</span>])).slice(1)}
+    </span>;
 
-function convertTex(texSrc: string, style: LatexStyle): Token[] {
-    const latexParser: LatexParser = new LatexParser(style);
-    return latexParser.parse(texSrc);
+function isArray(x: any): x is LaTeX[] {
+    return x.constructor === Array;
 }
 
-function renderTokens(parsed: Token[]): JSX.Element[] {
-    return parsed.map((token, i) => <li key={i}><TexNd node={token}/></li>);
-}
-
-function renderNodes(parsed: TexNode[]): JSX.Element[] {
-    return parsed.map((node, i) => <li key={i}><TexNd node={node}/></li>);
-}
-
-export class TexNd extends PureComponent<{ node: TexNode }, { isOpen: boolean }> {
-
-    constructor() {
-        super();
-        this.state = {isOpen: false};
-    }
-
-    toggle() {
-        this.setState({isOpen: !this.state.isOpen});
-    }
-
-    render() {
-        const {node} = this.props;
-        const childNodes: TexNode[] = node.childNodes;
-        return childNodes.length > 0 ?
-            <div className={"tex-node with-children" + (this.state.isOpen ? " open" : " closed")}>
-                <div onClick={() => this.toggle()} className="node-label">
-                    {node.toString()}
-                </div>
-                <ol className="node-children">
-                    {renderNodes(childNodes)}
-                </ol>
-            </div> : <div className={"tex-node without-children"}>
-                <div className="node-label">
-                    {node.toString()}
-                </div>
-            </div>;
+function renderTokens(parsed: LaTeX): React.ReactElement<any> {
+    if (isTeXComment(parsed))
+        return <span className={"TeXBlock " + parsed.type}>{parsed.text}<br/></span>;
+    else if (isTeXRaw(parsed))
+        return <span className={"TeXBlock " + parsed.type}>{parsed.text}</span>;
+    else if (isTeXComm(parsed) || isTeXEnv(parsed))
+        return <span className={"TeXBlock " + parsed.type}>{<span className="name">{parsed.name}</span>} (
+            <Arguments args={parsed.arguments}/>
+            )</span>;
+    else if (isTeXMath(parsed))
+        return <span className={"TeXBlock " + parsed.type}>$ {renderTokens(parsed.latex)} $</span>;
+    else if (isTeXLineBreak(parsed))
+        return <div className={"TeXLine"}><br/><br/></div>;
+    else if (isTeXBraces(parsed))
+        return <span className={"TeXBlock " + parsed.type}>{renderTokens(parsed.latex)}</span>;
+    else if (isFixArg(parsed))
+        return <span className={"TeXBlock " + parsed.type}>{renderTokens(parsed.latex)}</span>;
+    else if (isOptArg(parsed))
+        return <span className={"TeXBlock " + parsed.type}>{renderTokens(parsed.latex)}</span>;
+    else if (isArray(parsed))
+        return <span>{parsed.map((e, i) => <span key={i}>{renderTokens(e)}</span>)}</span>;
+    else {
+        console.error("Don't know how to deal with TeX:");
+        console.error(parsed);
+        return <span>???</span>;
     }
 }
 
-export const ParsedTex: React.StatelessComponent<{tex: string, style: LatexStyle}> = ({tex, style}) => {
-    const parsed: Token[] = convertTex(tex, style);
+// function renderNodes(parsed: LaTeX) {
+//     return <li/>; // parsed.map((node, i) => <li key={i}><TexNd node={node}/></li>);
+// }
+//
+// export class TexNd extends PureComponent<{ node: LaTeX }, { isOpen: boolean }> {
+//
+//     constructor() {
+//         super();
+//         this.state = {isOpen: false};
+//     }
+//
+//     toggle() {
+//         this.setState({isOpen: !this.state.isOpen});
+//     }
+//
+//     render() {
+//         const {node} = this.props;
+//         return childNodes.length > 0 ?
+//             <div className={"tex-node with-children" + (this.state.isOpen ? " open" : " closed")}>
+//                 <div onClick={() => this.toggle()} className="node-label">
+//                     {node.toString()}
+//                 </div>
+//                 <ol className="node-children">
+//                     {renderNodes(childNodes)}
+//                 </ol>
+//             </div> : <div className={"tex-node without-children"}>
+//                 <div className="node-label">
+//                     {node.toString()}
+//                 </div>
+//             </div>;
+//     }
+// }
 
+export const ParsedTex: React.StatelessComponent<{ tex: LaTeX[] }> = ({tex}) => {
     return <div className="tex-ast">
-        <ol className="node-children token-children">
-            {renderTokens(parsed)}
-        </ol>
+        <div className="node-children token-children">
+            {tex.map((latexBlock, i) => <span key={i}>{renderTokens(latexBlock)}</span>)}
+        </div>
     </div>;
 };
--- src/ts/components/TexTranslator.tsx
+++ src/ts/components/TexTranslator.tsx
@@ -1,99 +1,96 @@
 import * as React from "react";
 import {
-    LatexStyle,
-    PackageProperties,
-    mustBePackageProperties
+    isOk,
+    latexParser
 } from "latex-parser";
 import {
     ParsedTex
 } from "./ParsedTex";
-import {isError} from "tslint/lib/error";
 
 export interface TexState {
     tex: string;
-    styleSrc: string;
-    style: LatexStyle | Error;
 }
 
 export interface TexProps {
 }
 
-
-const defaultStyle: PackageProperties = {
-    symbols: [{
-        pattern: "\\\\"
-    }],
-    commands: [{
-        name: "author",
-        pattern: "[#1]#2",
-        modes: {TEXT: true},
-        parameters: [{}, {}],
-        operations: []
-    }, {
-        name: "author",
-        pattern: " [#1]#2",
-        modes: {TEXT: true},
-        parameters: [{}, {}],
-        operations: []
-    }, {
-        name: "author",
-        pattern: "#1",
-        modes: {TEXT: true},
-        parameters: [{}],
-        operations: []
-    }, {
-        name: '"',
-        pattern: "[#1]#2",
-        modes: {TEXT: true},
-        parameters: [{}, {}],
-        operations: []
-    }, {
-        name: '"',
-        pattern: " [#1]#2",
-        modes: {TEXT: true},
-        parameters: [{}, {}],
-        operations: []
-    }, {
-        name: '"',
-        pattern: "#1",
-        modes: {TEXT: true},
-        parameters: [{}],
-        operations: []
-    }, {
-        name: "document",
-        modes: {TEXT: true}
-    }, {
-        name: "enddocument",
-        modes: {TEXT: true}
-    }],
-    environments: [{
-        name: "document",
-        modes: {TEXT: true}
-    }]
-};
-
-function parseStyleFromProperties(packageProperties: PackageProperties): LatexStyle {
-    const latexStyle = new LatexStyle();
-    latexStyle.loadPackage("latex", packageProperties);
-    return latexStyle;
-}
-
-function parseStyle(styleSrc: string): LatexStyle | Error {
-    try {
-        return parseStyleFromProperties(mustBePackageProperties(JSON.parse(styleSrc)));
-    } catch (e) {
-        return e;
-    }
-}
+/*
+ const defaultStyle = {
+ symbols: [{
+ pattern: "\\\\"
+ }],
+ commands: [{
+ name: "author",
+ pattern: "[#1]#2",
+ modes: {TEXT: true},
+ parameters: [{}, {}],
+ operations: []
+ }, {
+ name: "author",
+ pattern: " [#1]#2",
+ modes: {TEXT: true},
+ parameters: [{}, {}],
+ operations: []
+ }, {
+ name: "author",
+ pattern: "#1",
+ modes: {TEXT: true},
+ parameters: [{}],
+ operations: []
+ }, {
+ name: '"',
+ pattern: "[#1]#2",
+ modes: {TEXT: true},
+ parameters: [{}, {}],
+ operations: []
+ }, {
+ name: '"',
+ pattern: " [#1]#2",
+ modes: {TEXT: true},
+ parameters: [{}, {}],
+ operations: []
+ }, {
+ name: '"',
+ pattern: "#1",
+ modes: {TEXT: true},
+ parameters: [{}],
+ operations: []
+ }, {
+ name: "document",
+ modes: {TEXT: true}
+ }, {
+ name: "enddocument",
+ modes: {TEXT: true}
+ }],
+ environments: [{
+ name: "document",
+ modes: {TEXT: true}
+ }]
+ };
+ */
+// function parseStyleFromProperties(packageProperties: PackageProperties): LatexStyle {
+//     const latexStyle = new LatexStyle();
+//     latexStyle.loadPackage("latex", packageProperties);
+//     return latexStyle;
+// }
+//
+// function parseStyle(styleSrc: string): LatexStyle | Error {
+//     try {
+//         return parseStyleFromProperties(mustBePackageProperties(JSON.parse(styleSrc)));
+//     } catch (e) {
+//         return e;
+//     }
+// }
 
 export class TexTranslator extends React.PureComponent<TexProps, TexState> {
     constructor() {
         super();
 
         this.state = {
-            tex: `an   \\author[optional]{author name}`,
-            styleSrc: JSON.stringify(defaultStyle, undefined, 2),
-            style: parseStyleFromProperties(defaultStyle)
+            tex: `an   \\author[optional arg]{author name} and % a comment
+more text`,
+            // styleSrc: JSON.stringify(defaultStyle, undefined, 2),
+            // style: parseStyleFromProperties(defaultStyle)
         };
     }
 
@@ -101,17 +98,7 @@ export class TexTranslator extends React.PureComponent<TexProps, TexState> {
         this.setState(
             {
                 tex,
-                style: this.state.style
-            }
-        );
-    }
-
-    onChangeStyle(styleSrc: string) {
-        this.setState(
-            {
-                styleSrc,
-                style: parseStyle(styleSrc),
-                tex: this.state.tex
+                // style: this.state.style
             }
         );
     }
@@ -120,21 +107,6 @@ export class TexTranslator extends React.PureComponent<TexProps, TexState> {
         // rows="8"
         //                   cols={40}
         return <div className="demo">
-            <section className="input-field-style">
-                <h2>TeX style</h2>
-                <div className="mdc-textfield mdc-textfield--multiline">
-                <textarea
-                    className="mdc-textfield__input input"
-                    value={this.state.styleSrc}
-                    name="style-input"
-                    rows={8}
-                    onChange={e => this.onChangeStyle(e.target.value)}
-                    id="style-input"
-                    placeholder="Input TeX style here"
-                />
-                </div>
-
-            </section>
             <section className="input-field-tex">
                 <h2>TeX source</h2>
                 <div className="mdc-textfield mdc-textfield--multiline">
@@ -151,26 +123,21 @@ export class TexTranslator extends React.PureComponent<TexProps, TexState> {
             </section>
             <section className="output">
                 <h2>Parsed TeX</h2>
-                {getParsedTex(this.state.style, this.state.tex)}
+                {getParsedTex(this.state.tex)}
             </section>
         </div>;
     }
 }
 
-function getParsedTex(style: LatexStyle | Error, tex: string) {
-    if (isError(style)) {
-        return <div className="error">
-            {"ERROR: " + style.message}
-        </div>;
-    }
-    try {
+function getParsedTex(tex: string) {
+    const result = latexParser.parse(tex);
+    if (isOk(result)) {
         return <ParsedTex
-            style={style}
-            tex={tex}
+            tex={result.value}
         />;
-    } catch (e) {
+    } else {
         return <div className="error">
-            {"ERROR: " + e.message}
+            {"ERROR: " + result.expected}
         </div>;
     }
 }
