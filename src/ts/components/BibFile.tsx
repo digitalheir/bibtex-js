fe008dfdcdbdc0a0819be6314096cfd014c9e1f6
Maarten Trompper <maartentrompper@freedom.nl>
2017-08-09 19:44:10 +0200

    1944

--- src/ts/components/BibFile.tsx
+++ src/ts/components/BibFile.tsx
@@ -1,27 +1,98 @@
 import * as React from "react";
 
 import {
-    BibComment, BibEntry, BibFile,
-    FieldValue, isBibComment,
+    BibComment,
+    BibFile,
+    BibEntry,
+    FieldValue,
+    AuthorName,
+    isAuthors, isBibComment,
     isBibEntry, isBibStringComponent, isPreamble,
     NonBibComment,
     toStringBibStringData
 } from "bibtex";
-import {StatelessComponent} from "react";
+import {ReactElement, StatelessComponent} from "react";
 
-function isArray(x: any): x is any[] {
-    return x.constructor === Array;
-}
+// function isArray(x: any): x is any[] {
+//     return x.constructor === Array;
+// }
 
+const AuthorComponent: StatelessComponent<{ author: AuthorName }> = ({author}) => {
+    const names: ReactElement<any>[] = [];
+    if (author.firstNames.length > 0)
+        names.push(<tr key="firstNames">
+            <td>First names</td>
+            <td>{
+                author.firstNames.map((n, i) =>
+                    <span key={i} className="author-firstName">{n}</span>
+                )
+            } ({author.initials.map((n, i) =>
+                <span key={i} className="author-initial">{n}</span>
+            )})
+            </td>
+        </tr>);
+
+    if (author.vons.length > 0)
+        names.push(<tr key="vons">
+            <td>von</td>
+            <td>{
+                author.vons.map((n, i) =>
+                    <span key={i} className="author-von">{n}</span>
+                )
+            }
+            </td>
+        </tr>);
+    if (author.lastNames.length > 0)
+        names.push(<tr key="lastNames">
+            <td>Last names</td>
+            <td>{
+                author.lastNames.map((n, i) =>
+                    <span key={i} className="author-lastName">{n}</span>
+                )
+            }
+            </td>
+        </tr>);
+    if (author.jrs.length > 0)
+        names.push(<tr key="jrs">
+            <td>Jr</td>
+            <td>{
+                author.jrs.map((n, i) =>
+                    <span key={i} className="author-jr">{n}</span>
+                )
+            }
+            </td>
+        </tr>);
+
+    return <div className="author-names">
+        <div className="author-id">{author.id}</div>
+        <table>
+            <tbody>
+            {names}
+            </tbody>
+        </table>
+    </div>;
+};
 const ValueCell: StatelessComponent<{ entry: FieldValue }> = ({entry}) => {
-    let newVar = "";
-    if (isBibStringComponent(entry))
-        newVar = toStringBibStringData(entry.data);
-    else newVar = entry + "";
-
-    return <td className="field-value">{
-        newVar
-    }</td>;
+    if (isAuthors(entry)) {
+        console.log("AUTHRs");
+        return <td className="field-value">
+            <ul>{
+                entry.authors$
+                    .map((a, i) => <li key={i}>{
+                        <AuthorComponent author={a}/>
+                    }</li>)
+            }</ul>
+        </td>;
+    }
+    else {
+        let newVar = "";
+        if (isBibStringComponent(entry))
+            newVar = toStringBibStringData(entry.data);
+        else newVar = entry + "";
+        return <td className="field-value">{
+            newVar
+        }</td>;
+    }
 };
 
 const BibEntryComponent: StatelessComponent<{ entry: BibEntry }> = ({entry}) => {
@@ -37,7 +108,7 @@ const BibEntryComponent: StatelessComponent<{ entry: BibEntry }> = ({entry}) => 
             </tr>)}
             </tbody>
         </table>
-        </div>;
+    </div>;
 };
 
 function renderEntry(entry: (NonBibComment | BibComment)): React.ReactElement<any> {
@@ -96,7 +167,7 @@ function renderEntry(entry: (NonBibComment | BibComment)): React.ReactElement<an
 export const BibFileComponent: React.StatelessComponent<{ bib: BibFile }> = ({bib}) => {
     return <div className="bibfile">
         <ul className="biblist">
-            {bib.content.map((entry, i) => <li key={i}>{renderEntry(entry)}</li>)}
+            {Object.keys(bib.entries$).map((entry, i) => <li key={i}>{renderEntry(bib.getEntry(entry))}</li>)}
         </ul>
     </div>;
 };
